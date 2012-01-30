59432309c2bdacd1b32ab2eb769e73b8b6a771c5
Michael <michael.beenen@gmail.com>
2012-01-29 20:41:36 -0800

    Clean up some old console logging

--- download-cloud-drive-directory.user.js
+++ download-cloud-drive-directory.user.js
@@ -9,7 +9,6 @@ if(unsafeWindow.console){
   var GM_log = unsafeWindow.console.log;
 }
 
-GM_log('script loaded');
 var delayFactor = 100;
 
 function mouseEvent(parent, type) {
@@ -31,49 +30,32 @@ function clickLink(target) {
 }
 
 var downloadItem = function(checkbox, index) {
-  GM_log('clicking ' + index);
   $(checkbox).trigger('click');
   setTimeout(function() {
-    GM_log('clicking download button for ' + index);
     var downloadLink = document.getElementById("download_button");
     clickLink(downloadLink);
-    //unsafeWindow.download(true);//$('#download_button').trigger('click');
   }, delayFactor * 4);
   setTimeout(function() {
-    GM_log('unchecking ' + index);
     $(checkbox).trigger('click');
   }, delayFactor * 8);
   return false;
 };
 
 $(document).ready(function () {
-  GM_log('document ready');
-  $('<a href="https://www.amazon.com/clouddrive/#G=0&path=/Music/Arcade+Fire" id="d_dir" style="display: inline-block;">Download Dir<span> </span></a>')
+  $('<a href="#" style="display: inline-block;" id="d_dir">Download Dir<span> </span></a>')
     .addClass("action search_action folder_action ScndryBtnMed")
     .insertAfter('#more_actions_button');
-
-  GM_log('button inserted');
 
   $('#d_dir').bind('click', function() {
     var total_file_count = $('.amazon_mp3_file').length;
     var i = 0;
-    GM_log('total count is ' + total_file_count)
-    GM_log('in click handler');
     $('#file_list_content input[type="checkbox"]').each(function() {
-      GM_log('setting timeout for' + i++);
       var button = this;
       var count = i;
       setTimeout(function() {
-        GM_log('count in inner function is' + count);
         downloadItem(button, count);
       }, delayFactor * 10 * (i-1));
     });
-    // for (i = 0; i < total_file_count; i++) {
-    //   var box = $('.amazon_mp3_file').get(i);
-    //   GM_log('setting timeout for' + i);
-    //   box.addClass('what!');
-    //   //setTimeout(downloadItem(box, i), 10000 * (i + 1));
-    // }
     return false;
   });
 });
